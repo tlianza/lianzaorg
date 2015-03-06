7ff760386c8b38315eb989bcd34360e14829914c
Tom Lianza <tom@lianza.org>
2015-03-06 10:30:05 -0800

    Adding icon banner to the top. Giving some spacing to the rebelmouse stuff.

--- index.php
+++ index.php
@@ -3,12 +3,28 @@
     require('_header.php');
 ?>
 
-Me:
-<a href="http://www.facebook.com/people/Tom_Lianza/4810243" rel="me" target="_blank">facebook</a> |
-<a href="http://www.linkedin.com/in/tlianza" rel="me" target="_blank">linkedin</a> |
-<a href="https://github.com/tlianza" rel="me" target="_blank">github</a> |
-<a href="http://stackoverflow.com/users/26624/tlianza" rel="me" target="_blank">stackoverflow</a> |
-<a href="https://plus.google.com/+TomLianza/" rel="me" target="_blank">google</a>
+<div class="clearfix content-heading">
+    Me: {
+    <a href="http://www.facebook.com/people/Tom_Lianza/4810243" rel="me" target="_blank">
+        <img src="/images/icons/FB-f-Logo__blue_29.png" />facebook
+    </a>
+    <a href="http://www.linkedin.com/in/tlianza" rel="me" target="_blank">
+        <img src="/images/icons/In-2C-14px.png" />linkedin
+    </a>
+    <a href="https://github.com/tlianza" rel="me" target="_blank">
+        <img src="/images/icons/GitHub-Mark-32px.png" />
+        github
+    </a>
+    <a href="http://stackoverflow.com/users/26624/tlianza" rel="me" target="_blank">
+        <img src="/images/icons/stackoverflow.png" />
+        stackoverflow
+    </a>
+    <a href="https://plus.google.com/+TomLianza/" rel="me" target="_blank">
+        <img src="/images/icons/google_plus.png" />
+        google
+    </a>
+    &nbsp;}
+</div>
 
 <div id="embed_rebel">
     <script type="text/javascript" class="rebelmouse-embed-script" src="https://www.rebelmouse.com/static/js-build/embed/embed.js?site=tlianza&height=1500&flexible=1&dont_load_new_posts=0&more_button=0&show_rebelnav=1"></script>
