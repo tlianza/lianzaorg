5a7661a03c039671e116864bd2606dee7643f5b0
Tom Lianza <tom@lianza.org>
2018-10-06 13:48:45 -0700

    Updated home page links to be canonical and https

--- index.php
+++ index.php
@@ -5,25 +5,21 @@
 
 <div class="clearfix content-heading">
     Me: {
-    <a href="http://www.facebook.com/people/Tom_Lianza/4810243" rel="me" target="_blank">
+    <a href="https://www.facebook.com/tlianza" rel="me" target="_blank">
         <img src="/images/icons/FB-f-Logo__blue_29.png" />facebook
     </a>
-    <a href="http://www.linkedin.com/in/tlianza" rel="me" target="_blank">
+    <a href="https://www.linkedin.com/in/tlianza/" rel="me" target="_blank">
         <img src="/images/icons/In-2C-14px.png" />linkedin
     </a>
     <a href="https://github.com/tlianza" rel="me" target="_blank">
         <img src="/images/icons/GitHub-Mark-32px.png" />
         github
     </a>
-    <a href="http://stackoverflow.com/users/26624/tlianza" rel="me" target="_blank">
+    <a href="https://stackoverflow.com/users/26624/tom-lianza" rel="me" target="_blank">
         <img src="/images/icons/stackoverflow.png" />
         stackoverflow
     </a>
-    <a href="https://plus.google.com/+TomLianza/" rel="me" target="_blank">
-        <img src="/images/icons/google_plus.png" />
-        google
-    </a>
-    <a href="http://lianza.org/blog" rel="me" target="_blank">
+    <a href="https://lianza.org/blog" rel="me" target="_blank">
         <img src="/images/icons/wpmini-blue.png" />
         blog
     </a>
